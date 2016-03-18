f681c008c8f3802e450137b49b55615a1a5b8c71
marcosocon <oconmarcos97@gmail.com>
2016-03-18 17:15:47 -0300

    setting up environment

---
+++ src/js/client.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Contact-List App.</h1>
+      </div>
+      )
+  }
+}
+React.render( <App />, document.getElementById("app"));
